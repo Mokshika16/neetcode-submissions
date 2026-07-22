045928248ab38f9708c3d82676f49e60098a4f15
Mokshika16 <mokshikagaur2005@gmail.com>
2026-07-23 01:57:39 +0530

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+
+        while b != 0:
+            carry = (a & b) << 1
+            a = (a ^ b) & mask
+            b = carry & mask
+
+        # Convert unsigned to signed
+        if a > 0x7FFFFFFF:
+            a = ~(a ^ mask)
+
+        return a
